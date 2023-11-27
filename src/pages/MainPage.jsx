ccc5513fc8ff87795e6dd76cfe60cb6c14356ce8
IrynaShashko <95182459+IrynaShashko@users.noreply.github.com>
2023-11-27 15:02:35 +0200

    add MainPage

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import slide1 from "../images/1.png";
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1 style={{ color: "#000" }}>MainPage</h1>
+      <img width="100%" src={slide1} alt="slide1" />
+    </div>
+  );
+};
+
+export default MainPage;
