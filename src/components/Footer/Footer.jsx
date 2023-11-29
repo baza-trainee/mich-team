21239b267477370eba86063f1db68e365cae3a32
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2023-11-29 11:57:08 +0000

    feat:add some styles to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import SubscribeForm from '../SubscribeForm/SubscribeForm';
-import { StyledLink } from './Footer.styled';
+import {
+  StyledLink,
+  FooterNav,
+  StyledFooter,
+  FooterLogo,
+  FooterContainer,
+  FooterLink,
+} from './Footer.styled';
 
 const Footer = () => {
   return (
-      <div className="container">
-      <img
-        src="../../images/footer-logo.png"
-        alt="Логотип бригади"
-        width="119"
-        height="136" />
-      <nav className='footer-nav'>
-        <StyledLink to="/merch">МЕРЧ</StyledLink>
-        <StyledLink to="">ДОСТАВКА І ОПЛАТА</StyledLink>
-        <StyledLink to="">ОБМІН І ПОВЕРНЕННЯ</StyledLink>
-        <a href='' target="_blank" rel="noopener noreferrer">mich_team</a>
- <a href="mailto:mich_team@gmail.com">mich_team@gmail.com</a>
-      <StyledLink to="">ПАРТНЕРИ</StyledLink>
-      </nav>
-      <SubscribeForm />
-    </div>
+    <StyledFooter>
+      <FooterContainer>
+        <FooterLogo
+          src="../../images/footer-logo.png"
+          alt="Логотип бригади"
+          width="119"
+          height="136"
+        />
+        <FooterNav>
+          <StyledLink to="/merch">МЕРЧ</StyledLink>
+          <StyledLink to="">ДОСТАВКА І ОПЛАТА</StyledLink>
+          <StyledLink to="">ОБМІН І ПОВЕРНЕННЯ</StyledLink>
+          <FooterLink
+            href="https://instagram.com/mich_team?igshid=MzMyNGUyNmU2YQ=="
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            mich_team
+          </FooterLink>
+          <FooterLink href="mailto:mich_team@gmail.com">
+            mich_team@gmail.com
+          </FooterLink>
+          <StyledLink to="">ПАРТНЕРИ</StyledLink>
+        </FooterNav>
+        <SubscribeForm />
+      </FooterContainer>
+    </StyledFooter>
   );
 };
 
--- src/components/Footer/Footer.styled.jsx
+++ src/components/Footer/Footer.styled.jsx
@@ -1,11 +1,39 @@
 import styled from '@emotion/styled';
 
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
+
+export const FooterLogo = styled.img`
+  display: block;
+  margin-bottom: 20px;
+`;
+export const StyledFooter = styled.footer`
+  background-color: #110003;
+  color: #fdfdfd;
+  padding: 15px;
+`;
+
+export const FooterContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const FooterNav = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #fdfdfd;
+  margin-bottom: 20px;
+`;
 
 export const StyledLink = styled(NavLink)`
-  color: black;
+  color: #fdfdfd;
+  margin-bottom: 20px;
 
-  &.active {
+  /* &.active {
     color: orange;
-  }
+  } */
+`;
+export const FooterLink = styled.a`
+  color: #fdfdfd;
+  margin-bottom: 20px;
 `;
