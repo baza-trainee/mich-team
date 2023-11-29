b5ba800a2e72d0b841649c22736fa43569ef3626
Malvina1993 <mykhamalvina@gmail.com>
2023-11-29 22:46:25 +0200

    added checkboxes and buttons

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import slide1 from '../images/1.png';
 import ProductList from '../components/ProductList/ProductList';
+import RegisterPage from './RegisterPage/RegisterPage';
 
 const MainPage = () => {
   return (
