a3eeb20726d1054f90514c156cb02d839dc4dbed
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2023-11-29 09:06:35 +0000

    feat:add logo, Subscribe form

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,32 @@ import React from 'react';
 import SubscribeForm from '../SubscribeForm/SubscribeForm';
 
 const Footer = () => {
-  return <SubscribeForm />;
+  return (
+    <div className="container">
+      <img
+        src="../../images/footer-logo.png"
+        alt="Логотип бригади"
+        width="119"
+        height="136"
+      />
+      <ul>
+        <li>
+          <link></link>
+        </li>
+        <li>
+          <link></link>
+        </li>
+        <li>
+          <link></link>
+        </li>
+        <li>
+          <link></link>
+              </li>
+              
+      </ul>
+      <SubscribeForm />
+    </div>
+  );
 };
 
 export default Footer;
