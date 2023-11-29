d2140b51e51d751b54678917b96a73fde949cc2e
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2023-11-29 11:20:37 +0000

    fix:subscribe form

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,23 @@
 import React from 'react';
 import SubscribeForm from '../SubscribeForm/SubscribeForm';
+import { StyledLink } from './Footer.styled';
 
 const Footer = () => {
   return (
-    <div className="container">
+      <div className="container">
       <img
         src="../../images/footer-logo.png"
         alt="Логотип бригади"
         width="119"
-        height="136"
-      />
-      <ul>
-        <li>
-          <link></link>
-        </li>
-        <li>
-          <link></link>
-        </li>
-        <li>
-          <link></link>
-        </li>
-        <li>
-          <link></link>
-              </li>
-              
-      </ul>
+        height="136" />
+      <nav className='footer-nav'>
+        <StyledLink to="/merch">МЕРЧ</StyledLink>
+        <StyledLink to="">ДОСТАВКА І ОПЛАТА</StyledLink>
+        <StyledLink to="">ОБМІН І ПОВЕРНЕННЯ</StyledLink>
+        <a href='' target="_blank" rel="noopener noreferrer">mich_team</a>
+ <a href="mailto:mich_team@gmail.com">mich_team@gmail.com</a>
+      <StyledLink to="">ПАРТНЕРИ</StyledLink>
+      </nav>
       <SubscribeForm />
     </div>
   );
