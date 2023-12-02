2721ded85b308d6e0a66b025e769b963fb5941b0
Malvina1993 <81303915+Malvina1993@users.noreply.github.com>
2023-12-02 09:34:55 +0200

    Register page (#32)
    * changed Button as link
    
    * added all elements of page

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,27 +1,14 @@
 import React from "react"
-import { BtnNetwork, BtnSignIn, CheckbBoxDiv, LabelInput, LinkDocument, RegistForm, RegistInput, RegistInputCheck, RegisterDivInput, SpanElem } from "./RegisterForm.styled";
-import { useEffect } from "react";
-import { useState } from "react";
+import { BtnSignIn, CheckbBoxDiv, LabelInput, LinkDocument,RegistForm, RegistInput, RegistInputCheck,RegisterDivInput, RegisterTitle, SpanElem } from "./RegisterForm.styled";
 
-export const RegisterForm = () => {
-    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setScreenWidth(window.innerWidth);
-    };
-
-    // Add a listener to update screenWidth on resize
-    window.addEventListener('resize', handleResize);
-
-    // Remove the listener when the component is unmounted
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+const RegisterForm = () => {
+    
 
     return (
-        <RegistForm action="">
+      <RegistForm action="">
+            <RegisterTitle>РЕЄСТРАЦІЯ</RegisterTitle>
+
             <RegistInput type="email" placeholder="E-mail"/>
             <RegistInput type="password" placeholder="Пароль"/>
             <RegistInput type="password" placeholder="Повторіть пароль" />
@@ -29,23 +16,24 @@ export const RegisterForm = () => {
             <CheckbBoxDiv>
                 <RegisterDivInput>
                     <RegistInputCheck type="checkbox" id="rememb" /> 
-                    <LabelInput for="rememb">Запам&rsquo;ятати мене </LabelInput>
+                    <LabelInput htmlFor="rememb">Запам&rsquo;ятати мене </LabelInput>
                 </RegisterDivInput>
                 <RegisterDivInput>
                     <RegistInputCheck type="checkbox" id="subckribe"/> 
-                    <LabelInput for="subckribe">Так, я хочу підписатися на розсилку та отримувати інформацію про оновлення </LabelInput>
+                    <LabelInput htmlFor="subckribe">Так, я хочу підписатися на розсилку та отримувати інформацію про оновлення </LabelInput>
                 </RegisterDivInput>
                 <RegisterDivInput>
                     <RegistInputCheck type="checkbox" id="agrre"/> 
-                    <LabelInput for="agrre">Я згоден з <LinkDocument>Правилами користування сайтом</LinkDocument><SpanElem>*</SpanElem></LabelInput>
+                    <LabelInput htmlFor="agrre">Я згоден з <LinkDocument>Правилами користування сайтом</LinkDocument><SpanElem>*</SpanElem></LabelInput>
                 </RegisterDivInput>
             </CheckbBoxDiv>
 
             <BtnSignIn>ЗАРЕЄСТРУВАТИСЯ</BtnSignIn>
-            {(screenWidth<768)?<BtnNetwork>УВІЙТИ З GOOGLE</BtnNetwork>:<BtnNetwork>УВІЙТИ ЗА ДОПОМОГОЮ GOOGLE</BtnNetwork>}
-            {(screenWidth < 768) ? <BtnNetwork>УВІЙТИ З FACEBBOK</BtnNetwork> : <BtnNetwork>УВІЙТИ ЗА ДОПОМОГОЮ FACEBBOK</BtnNetwork>}
             
+        
         </RegistForm>
         
     )
 };
+
+export default RegisterForm
