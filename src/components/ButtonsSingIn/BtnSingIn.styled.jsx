9471e84cfc31e15c3650b83a59468a3a877e48f9
natalia646 <75090544+natalia646@users.noreply.github.com>
2023-12-04 12:52:12 +0200

    Feature/no ref/animation of elements (#36)
    * feature: added eyes svg
    
    * feat: added animation to buttoms
    
    * feat: animation input

--- src/components/ButtonsSingIn/BtnSingIn.styled.jsx
+++ src/components/ButtonsSingIn/BtnSingIn.styled.jsx
@@ -1,39 +1,60 @@
 import styled from '@emotion/styled';
 
-
-
 export const SingInGoogle = styled.button`
+  color: #110003;
+  border: 1px solid #b3b3b3;
+
   height: 3.5rem;
   width: 100%;
-  padding: 1rem 4.5rem 1rem 1rem;
   border-radius: 0.25rem;
   font-family: Play;
+  margin: 1.5rem 0;
   font-size: 1.375rem;
   font-weight: 700;
   background-color: #fff;
+  cursor: pointer;
+  text-transform: uppercase;
+  padding: 1rem;
+  display: grid;
+  text-align: center;
+  grid-template-columns: 0.1fr 1fr;
 
-  border: 1px solid #b3b3b3;
-  color: #110003;
-  margin: 1.5rem 0;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  :hover {
+    box-shadow: 2px 4px 4px #b3b3b3;
+  }
+  :active {
+    transform: translateY(2px);
+  }
 
+  @media screen and (max-width: 768px) {
+  }
 `;
 
 export const SingInFacebook = styled.button`
+  border: 1px solid #1877f2;
+  color: #1877f2;
+
   height: 3.5rem;
   width: 100%;
-  padding: 1rem 3.5rem 1rem 1rem;
   border-radius: 0.25rem;
   font-family: Play;
   font-size: 1.375rem;
   font-weight: 700;
-
   background-color: #fff;
-  border: 1px solid #1877f2;
-  color: #1877f2;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  cursor: pointer;
+  text-transform: uppercase;
+  padding: 1rem;
+  display: grid;
+  text-align: center;
+  grid-template-columns: 0.1fr 1fr;
+
+  :hover {
+    box-shadow: 2px 4px 4px #b3b3b3;
+  }
+  :active {
+    transform: translateY(2px);
+  }
+
+  @media screen and (max-width: 768px) {
+  }
 `;
--- src/components/ButtonsSingIn/BtnSingInFacebook.jsx
+++ src/components/ButtonsSingIn/BtnSingInFacebook.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SingInFacebook } from './BtnSingIn.styled';
 import facebook from '../../icons/facebook.svg';
 
 const BtnSingInFacebook = () => {
-  return (
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return screenWidth < 768 ? (
+    <SingInFacebook>
+      <img src={facebook} alt="facebook"></img>
+      Увійти з facebook
+    </SingInFacebook>
+  ) : (
     <SingInFacebook>
       <img src={facebook} alt="facebook"></img>
       Увійти за допомогою facebook
