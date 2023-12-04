d97e537ae37e03b6095f9dcae3c7649f6085e8ad
Malvina1993 <81303915+Malvina1993@users.noreply.github.com>
2023-12-04 23:04:33 +0200

    Added svg to input

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ProductList from '../components/ProductList/ProductList';
 import Hero from '../components/Hero/Hero';
+import RegisterPage from './RegisterPage/RegisterPage';
 
 const MainPage = () => {
   return (
     <div>
       <Hero />
       <ProductList />
+      <RegisterPage/>
     </div>
   );
 };
