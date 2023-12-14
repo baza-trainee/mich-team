8bde89d0825158babb06ae6e83ece69fcbd7f46a
Mariana Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2023-12-14 10:19:47 +0000

    add scrollbar, auto scroll (#50)
    * add scrollbar, auto scroll
    
    * fix auto hide duration
    
    * fix: change auto hide duration
    
    * fix: change react-custom-scrollbars version
    
    * fix: add custon scroll bar 2

---
+++ src/components/Scrollbar/Scrollbar.jsx
@@ -0,0 +1,37 @@
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Scrollbars } from 'react-custom-scrollbars-2';
+
+const CustomScrollbar = ({ children }) => {
+  const scrollbarsRef = useRef(null);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [children]);
+
+  const scrollToBottom = () => {
+    if (scrollbarsRef.current) {
+      const { scrollHeight, clientHeight } = scrollbarsRef.current;
+      const maxScrollTop = scrollHeight - clientHeight;
+      scrollbarsRef.current.scrollTop(maxScrollTop);
+    }
+  };
+
+  return (
+    <Scrollbars
+      style={{ width: '100%', height: '100vh' }}
+      ref={scrollbarsRef}
+      autoHide
+      autoHideTimeout={1000}
+      autoHideDuration={200}
+    >
+      {children}
+    </Scrollbars>
+  );
+};
+
+CustomScrollbar.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default CustomScrollbar;
