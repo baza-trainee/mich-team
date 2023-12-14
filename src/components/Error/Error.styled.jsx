3f68267d11adc5df626e6c483b4052449590bfa6
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2023-12-14 11:53:56 +0000

    feature: add error component and styles

---
+++ src/components/Error/Error.styled.jsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  padding-bottom: 207px;
+  padding-top: 100px;
+`;
+export const Errortext = styled.p`
+  color: #000;
+
+  font-variant-numeric: lining-nums proportional-nums;
+  /* H11 */
+  font-family: Play;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 120%; /* 21.6px */
+
+  margin-top: 32px;
+  margin-bottom: 32px;
+`;
