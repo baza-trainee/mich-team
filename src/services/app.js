4d22a580dee5f52249586237ceae2a8273b17a0a
Malvina1993 <81303915+Malvina1993@users.noreply.github.com>
2023-12-21 01:34:20 +0200

    added checkbox checked

--- src/services/app.js
+++ src/services/app.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+ 
+
 
 const userInstance = axios.create({
   baseURL: 'https://8bc8-78-26-198-240.ngrok-free.app/user_auth/',
@@ -6,14 +8,34 @@ const userInstance = axios.create({
 
 })
 
+
 // export const setToken = (token) => {
 //     contactsInstance.defaults.headers.common.Authorization = `Bearer ${token}`
 // }
 
+// export const requestSignUpUser = async (newUserData) => {
+//     const { data } = await userInstance.post('/users/', newUserData);
+//     console.log(newUserData);
+//     console.log(data);
+//     return data;
+// };
+
 export const requestSignUpUser = async (newUserData) => {
-    const { data } = await userInstance.post('/users/', newUserData);
-    console.log(newUserData);
-    console.log(data);
-    return data;
+    try {
+        console.log(newUserData);
+        const config = {
+            headers: {
+                'ngrok-skip-browser-warning': 'hello',
+            },
+        };
+        const { data } = await userInstance.post('/users/', newUserData, config);
+        console.log(newUserData);
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error('Помилка при реєстрації користувача:', error);
+        throw error; 
+    }
 };
+
 
