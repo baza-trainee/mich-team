5e94c5ff8f7bbc49204a9cfa2de512f6f07abbed
Malvina1993 <81303915+Malvina1993@users.noreply.github.com>
2024-01-24 21:51:31 +0200

    added modal window

--- src/components/RegisterModalWindComponent/RegisterModalWindComponent.jsx
+++ src/components/RegisterModalWindComponent/RegisterModalWindComponent.jsx
@@ -1,25 +1,46 @@
 import React from "react";
 import PropTypes from 'prop-types';
-import { ButtonLink, CloseBtn, EmailIcon, MessageOfemail, MessageTitle, RegisterModalContent, RegisterModalWind } from "./RegisterModalWindComponent.style";
+import { ButtonLink, ButtonUserActive, CloseBtn, EmailIcon, MessageOfemail, MessageTitle, RegisterModalContent, RegisterModalWind } from "./RegisterModalWindComponent.style";
 import smsTracking from '../../icons/sms-tracking.svg';
 import closeBtn from '../../icons/close.svg';
+import checkIcon from '../../icons/checkIcon.svg'
+import { useNavigate } from "react-router-dom";
 
 
-const RegisterModalWindComponent = ({onChange}) => {
+const RegisterModalWindComponent = ({ onChange, isActive }) => {
+    
+    const history = useNavigate();
     
     const handleCloseModal = () => {
         const openWindow = false;
         onChange(openWindow);
         localStorage.removeItem('showModal');
+        history('/?activate=true');
+    }
+
+    const handleOnClick = () => {
+        const openWindow = false;
+        onChange(openWindow);
+        history('/');
     }
 
     return <RegisterModalWind>
-        <RegisterModalContent>
+        {(!isActive) ?
+        
+        (<RegisterModalContent>
             <ButtonLink onClick={handleCloseModal} type="button"><CloseBtn src={closeBtn} alt="closeBtn"></CloseBtn></ButtonLink>
             <EmailIcon src={smsTracking} />
-            <MessageTitle>Електронний лист надіслано!</MessageTitle>
+            <MessageTitle>ЕЛЕКТРОННИЙ ЛИСТ НАДІСЛАНО!</MessageTitle>
             <MessageOfemail>Для завершення реєстрації, будь ласка, перевірте свою електронну пошту та підтвердіть свою адресу, клікнувши на посилання у листі від нас</MessageOfemail>
-        </RegisterModalContent>
+        </RegisterModalContent>) :
+            
+        (<RegisterModalContent>
+            <EmailIcon src={checkIcon} />
+            <MessageTitle>РЕЄСТРАЦІЯ ЗАВЕРШЕНА УСПІШНО</MessageTitle>
+            <MessageOfemail>Тепер ви можете користуватися всіма можливостями нашого сайту.</MessageOfemail>
+            <ButtonUserActive onClick={handleOnClick} type="submit">ПЕРЕЙТИ ДО МАГАЗИНУ</ButtonUserActive>
+
+        </RegisterModalContent>)}
         
     </RegisterModalWind>
 }
@@ -27,7 +48,7 @@ const RegisterModalWindComponent = ({onChange}) => {
 
 RegisterModalWindComponent.propTypes = {
     onChange: PropTypes.func.isRequired,
-    
+    isActive: PropTypes.bool.isRequired
 };
 
 export default RegisterModalWindComponent;
--- src/components/RegisterModalWindComponent/RegisterModalWindComponent.style.jsx
+++ src/components/RegisterModalWindComponent/RegisterModalWindComponent.style.jsx
@@ -74,3 +74,61 @@ export const MessageOfemail = styled.p`
     line-height: 1.2;
 
 `
+
+export const ButtonUserActive = styled.button`
+position: relative;
+  display: inline-block;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--Main-red, #d91f2d);
+  color: var(--Wtite, #fdfdfd);
+  text-align: center;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 120%;
+  text-transform: uppercase;
+  width: 328px;
+  height: 50px;
+  padding: 12px 36px;
+  border-color: #d91f2d;
+  border-radius: 4px;
+  position: relative;
+  overflow: hidden;
+  z-index: 1;
+  transition: color 150ms ease-in-out;
+
+  &:after {
+    content: '';
+    position: absolute;
+    display: block;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 0;
+    height: 100%;
+    background: #000;
+    z-index: -1;
+    transition: width 150ms ease-in-out;
+  }
+
+  &:hover {
+    color: #fff;
+    &:after {
+      width: 110%;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    width: ${props => props.tabWidth};
+    height: 60px;
+  }
+  @media screen and (min-width: 1440px) {
+    width: 520px;
+    height: 60px;
+    padding: 16px 36px;
+    font-size: 22px;
+    line-height: 140%;
+  }
+`
